82d55e3f0040517786920a54fb4e549982b382b4
gamer13433 <53436256+gamer13433@users.noreply.github.com>
2020-05-11 14:59:26 +0500

    Update Android.mk

--- AdvancedDisplay/Android.mk
+++ AdvancedDisplay/Android.mk
@@ -20,7 +20,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v14-preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.havoc.platform.internal
+    havoc.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
